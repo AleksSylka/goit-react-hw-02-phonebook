d18610e3a78038031c03bc6a8de193e3eaed7614
AleksSylka <119104646+AleksSylka@users.noreply.github.com>
2023-06-11 14:19:33 +0300

    HW-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+/* import { nanoid } from 'nanoid'; */
+import Form from "./FormComp/Form";
+import Filter from "./Filter/Filter";
+import ConractList from "./ContactList/ContactList";
+import css from './app.module.css'
+
+class App extends Component {
+
+  state = {
+  contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    filter: ''
+  }
+
+  formData = data => {
+    if (this.state.contacts.find(({name}) => name === data.name)) {return window.alert(`${data.name} is already in contacts`)}
+    this.state.contacts.push(data)
+    this.setState(this.state.contacts)
+  }
+
+  filterInput = data => {
+    this.setState({filter: data})
+  }
+
+  deletContact = name => {
+    let i = this.state.contacts.findIndex(contact => contact.name === name);
+    this.setState(this.state.contacts.splice(i, 1));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        
+        <Form onSubmit={this.formData} />
+        
+        <h2>Contacts</h2>
+        
+        <Filter onChange={this.filterInput} />
+        
+        {this.state.contacts.length > 0 && (<ConractList arr={this.state.contacts.filter(({ name }) => (name.toLocaleLowerCase().includes(this.state.filter.toLocaleLowerCase())))}
+          onDeletContact={this.deletContact} />)}
+        
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,25 @@
+import ContactItem from "./ContactItem/ContactItem";
+import PropTypes from 'prop-types';
+import css from './contactList.module.css'
+
+const ConractList = ({arr, onDeletContact}) => (
+    <ul>
+        {arr.map(({ id, name, number }) => (
+            <ContactItem key={id}
+                name={name}
+                number={number}
+                onDeletContact={onDeletContact}
+            />))}
+
+    </ul>
+)
+
+export default ConractList
+
+ConractList.prototype = {
+  arr: PropTypes.array,
+  name: PropTypes.string,
+  number: PropTypes.string,
+  id: PropTypes.string,
+  onDeletContact: PropTypes.func
+}
